ca840a0c4d5de9f7c16075e16eef4eb06347777f
Sabohat Muzaffarova <94sabohat@gmail.com>
2016-06-30 20:01:08 -0400

    home work

---
+++ java.js
@@ -0,0 +1,34 @@
+alert("Welcome");
+	
+	console.log("Welcome")
+	
+
+	function logToHTML() {
+		document.getElementById("title").innerHTML ="Good Bye";
+	}
+
+ var colors = ["red", "blue", "yellow",]
+
+ console.log(colors[0]);
+ console.log(colors[1]);
+ console.log(colors[1]);
+
+
+var numbers = [23, 32, 100, -19]
+
+console.log(numbers[3]);
+console.log(numbers[2]);
+
+var colorsAndNumbers = [colors, numbers];
+console.log(colorsAndNumbers[1][3]);
+
+var SM = {
+	gender: "female",
+	nationality:"uzbek",
+	favorite_foods: "chipotle",
+};
+
+console.log(SM["gender"]);
+console.log(SM.nationality);
+console.log(SM.favorite_foods);
+
